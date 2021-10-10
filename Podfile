ce4e8e0b2c29b2885294b4ecf853ee00b0f8e05b
vincechen2 <Vincent.chen.1@stonybrook.edu>
2021-10-10 12:55:29 -0400

    first commit

---
+++ Podfile
@@ -0,0 +1,12 @@
+
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+target 'parstagram' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+  # Pods for Enstagram
+  pod 'Parse'
+  pod 'AlamofireImage'
+    inherit! :search_paths
+
+end
---
+++ parstagram/AppDelegate.swift
@@ -0,0 +1,45 @@
+//
+//  AppDelegate.swift
+//  parstagram
+//
+//  Created by Vincent Chen on 10/7/21.
+//
+
+import UIKit
+import Parse
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "T9NcEZk5u3ZMGckHdlfo7odsbTu2zpJCCt7qIZ8n" // <- UPDATE
+                    $0.clientKey = "4oQIVyg3ED1mslnICfYlh1ShUEaWA246EHnMCP8N" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
+        // Override point for customization after application launch.
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    // AppDelegate.swift
+
+
+
+}
+
